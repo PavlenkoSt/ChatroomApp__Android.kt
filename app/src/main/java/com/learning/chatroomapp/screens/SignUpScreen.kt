442ca1a135f8c352d4a4962cde7f0cbc8910abb7
PavlenkoSt <verbatim8872@gmail.com>
2024-08-28 22:41:21 +0300

    Cleanup

--- app/src/main/java/com/learning/chatroomapp/screens/SignUpScreen.kt
+++ app/src/main/java/com/learning/chatroomapp/screens/SignUpScreen.kt
@@ -10,10 +10,8 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
